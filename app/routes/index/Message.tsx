76d532c8b725008fb319a293fc107ee6952c9011
zifahm <muhamm3dhafiz@gmail.com>
2023-06-07 00:35:26 +0530

    fix(app): memoized and fixed reload issues

--- app/routes/index/Message.tsx
+++ app/routes/index/Message.tsx
@@ -1,11 +1,42 @@
-import { useRouteLoaderData } from "@remix-run/react";
+import { useRevalidator, useRouteLoaderData } from "@remix-run/react";
 import { Box, Flex, Text } from "gestalt";
-import { Fragment } from "react";
+import { Fragment, useEffect, useMemo } from "react";
+import { useEventSource } from "remix-utils";
 import type { RootData } from "../../root";
 import { Bubble, BubbleUser } from "./message/Bubbles";
 
 export default function Message() {
   const loaderData = useRouteLoaderData("root") as RootData;
+  const revalidator = useRevalidator();
+
+  let lastMessageId = useEventSource("/message", {
+    event: "new-message",
+  });
+
+  useEffect(() => revalidator.revalidate(), [lastMessageId]);
+
+  const chat = useMemo(
+    () =>
+      loaderData?.messages?.map((message) => (
+        <Fragment key={message.id}>
+          {message.userId === loaderData.user?.id ? (
+            <BubbleUser
+              message={message}
+              user={message.user}
+              view={loaderData.chatViewCount ?? 0}
+            />
+          ) : (
+            <Bubble
+              message={message}
+              user={message.user}
+              view={loaderData.chatViewCount ?? 0}
+            />
+          )}
+        </Fragment>
+      )),
+    [loaderData.chatViewCount, loaderData?.messages, loaderData.user?.id]
+  );
+
   return (
     <Box marginTop={3}>
       <Flex gap={6} direction="column">
@@ -14,23 +45,7 @@ export default function Message() {
             {loaderData.messageCount} chats
           </Text>
         </Flex>
-        {loaderData?.messages?.map((message) => (
-          <Fragment key={message.id}>
-            {message.userId === loaderData.user?.id ? (
-              <BubbleUser
-                message={message}
-                user={message.user}
-                view={loaderData.chatViewCount ?? 0}
-              />
-            ) : (
-              <Bubble
-                message={message}
-                user={message.user}
-                view={loaderData.chatViewCount ?? 0}
-              />
-            )}
-          </Fragment>
-        ))}
+        {chat}
         <Box />
       </Flex>
     </Box>
--- app/routes/index/index.tsx
+++ app/routes/index/index.tsx
@@ -1,6 +1,4 @@
-import { useRevalidator, useRouteLoaderData } from "@remix-run/react";
-import { useEffect } from "react";
-import { useEventSource } from "remix-utils";
+import { useRouteLoaderData } from "@remix-run/react";
 import SmallContainer from "../../components/SmallContainer";
 import type { RootData } from "../../root";
 import Chat from "./Chat";
@@ -9,14 +7,6 @@ import Message from "./Message";
 
 export default function () {
   const loaderData = useRouteLoaderData("root") as RootData;
-  const revalidator = useRevalidator();
-
-  let lastMessageId = useEventSource("/message", {
-    event: "new-message",
-  });
-
-  useEffect(() => revalidator.revalidate(), [lastMessageId]);
-
   if (
     !loaderData.messages ||
     loaderData.messages == undefined ||
